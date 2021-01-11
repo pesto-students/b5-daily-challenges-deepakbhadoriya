fe7f8b90b7d46aff916945a3e6df7788fdc7bb61
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2021-01-11 15:42:16 +0530

    obj deep copy done

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -4,11 +4,13 @@ const deepCopyObject = objToCopy => {
     // Create an array or object to hold the values
     const resultObject = Array.isArray(objToCopy) ? [] : {};
 
+    //To copy all properties
     for (const key of Object.getOwnPropertyNames(objToCopy)) {
         const value = objToCopy[key];
         resultObject[key] = deepCopyObject(value);
     }
 
+    //To copy symbol
     for (const key of Object.getOwnPropertySymbols(objToCopy)) {
         const value = objToCopy[key];
         resultObject[key] = deepCopyObject(value);
