45a689648780922e5ab67283cec0cbef0f9ff502
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2021-01-19 14:10:10 +0530

    added error handling

--- js-exercises/iter-infinite/iterInfinite.js
+++ js-exercises/iter-infinite/iterInfinite.js
@@ -1,6 +1,17 @@
-function* count(start = 0, step = 1, noOfTime = true) {
+// ? Instead of returning boolean throw error here, So don't have to repeat ERROR statement
+const ifNotNumThrowError = (...params) => {
+  for (const el of params) {
+    if (el !== undefined && typeof el !== 'number') {
+      throw new Error(`Received ${typeof el} | expecting NUMBER`);
+    }
+  }
+};
+
+function* count(start = 0, step = 1, noOfTime) {
+  ifNotNumThrowError(start, step, noOfTime);
   let result = start;
-  let iteration = noOfTime;
+  let iteration = true;
+  if (noOfTime) iteration = noOfTime;
   while (iteration) {
     yield result;
     result += step;
@@ -8,8 +19,11 @@ function* count(start = 0, step = 1, noOfTime = true) {
   }
 }
 
-function* cycle(iter, noOfTime = true) {
-  let iteration = noOfTime;
+function* cycle(iter, noOfTime) {
+  ifNotNumThrowError(noOfTime);
+  let iteration = true;
+  if (noOfTime) iteration = noOfTime;
+
   while (iteration) {
     for (const el of iter) {
       yield el;
@@ -18,8 +32,10 @@ function* cycle(iter, noOfTime = true) {
   }
 }
 
-function* repeat(value, noOfTime = true) {
-  let iteration = noOfTime;
+function* repeat(value, noOfTime) {
+  ifNotNumThrowError(noOfTime);
+  let iteration = true;
+  if (noOfTime) iteration = noOfTime;
   while (iteration) {
     yield value;
     if (typeof iteration === 'number') iteration -= 1;
